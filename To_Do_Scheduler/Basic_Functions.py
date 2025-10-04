31b27593da4110466df944b204bb7ce53cfc9888
PoppaP15 <purintea@rose-hulman.com>
2025-10-03 23:29:28 -0400

    Small edits

--- To_Do_Scheduler/Basic_Functions.py
+++ To_Do_Scheduler/Basic_Functions.py
@@ -26,48 +26,64 @@ mySchedule = Scheduler
 listOfProjects = []
 listOfTasks = []
 
+class Tasks:
+     
+     isTaskDone = False
+     durationMultiplier = 1.5
 
+     def __init__(self, name, due_date, project_type, duration):
+        ## Given the
 
-class todos:
-
-    isTaskDone = False
-    durationMultiplier = 1.5
-    
-    def __init__(self, due_date, project_type, duration):
-    ## Given the
-   
+        self.name = name
         self.due_date = due_date
         self.project_type = project_type
         self.duration = duration
+
+     def add_Task(self):
+         ## So I need to save the names, duedate, project type, and duration
+         ## They need to keep that information and when necessary be able to update that information
+         listOfTasks.append(self.name)
         
 
-    def find_due_date(self):
+     def find_due_date(self):
         ## Todo: from the front end communicate to python so I can find a date
         pass
          
 
-    def exepected_Duration(self):
+     def exepected_Duration(self):
         ## finds expected duration for a project because my brain be straight lying
         expected_duration = self.duration * mySchedule ## need to fix this this shit ain't working
         return expected_duration
 
 
-    def progress_Checkin_function(self):
+     def progress_Checkin_function(self):
         ## has a progress checkin based on duedate, projecttype and expected duration
+        ## if it is a task maybe only checkin to see if it is complete
         pass
 
-    def set_up_time(self):
+     def set_up_time(self):
         ## time to set something up really depending on project type and materials
+        ## I feel this is more project based than anything 
         pass
     
-    def close_Task(self):
+     def close_Task(self):
         isTaskDone = True
         return isTaskDone
+
+
+
+
+class todos(Tasks):
+    
+    def __init__(self):
+        super.__init__(self.name)
+
+   
     
 
 class projects: 
-    def add_Project(self):
-        pass
+    def __init__(self):
+        super.__init__(self.name)
 
 
 
