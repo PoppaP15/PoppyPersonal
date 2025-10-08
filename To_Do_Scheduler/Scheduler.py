fd6db8b2c304c6f7816f78eed09699b40d917332
ep5328 <ep5328@northamericanstainless.com>
2025-10-08 13:27:09 -0400

    Added add and remove functions to scheduler

--- To_Do_Scheduler/Scheduler.py
+++ To_Do_Scheduler/Scheduler.py
@@ -5,8 +5,34 @@ from Events import events
 from Other_Activities import otherActivities
 import math
 
+class Scheduler:
 
+    def __init__(self):
+        # self.myTask = task()
+        # self.myEvents = events()
+        # self.myOtherActivities = otherActivities()
+        ## may need a  month and year of something like that
+        pass
+# ## This definetly not right man
+#     def addTask(self):
+#         self.myTask = task()
+#         print("Added new Task {self.mytask.name}")
 
-myTask = task()
-myevents = events()
-myOtherActivities = otherActivities()
+#     def addEvent(self):
+#         self.myEvents = events()
+
+#     def addOtherActivities(self):
+#         self.myOtherActivities = otherActivities()
+
+#     def removeTask(self):
+#         self.myTask = task()
+#         print("Added new Task {self.mytask.name}")
+
+#     def removeEvent(self):
+#         self.myEvents = events()
+
+#     def removeOtherActivities(self):
+#         self.myOtherActivities = otherActivities()
+
+    
+
